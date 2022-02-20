07b73ae98180c7efb310565057304b5ba87fd999
Oleksii Lymonov <83865826+Oleksii25@users.noreply.github.com>
2022-02-20 14:26:27 +0200

    new-test

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,7 +1,16 @@
-import { useRouter, withRouter } from "next/router";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import React from "react";
 
 export default function Project() {
   const { query } = useRouter();
 
-  return <h1>Project A = {query.id}</h1>;
+  return (
+    <>
+      <h1>Project A = {query.id}</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </>
+  );
 }
